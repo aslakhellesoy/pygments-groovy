e521be07f251d07ae3879af6c0cccee6519623a6
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-11-15 04:46:11 +0100

    Some tweaks to make setup work

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Pygments Groovy Lexer plugin setup file for setuptools
+Groovy Pygments Lexer plugin setup file for setuptools
 """
 from setuptools import setup
 
 setup(
-    name='Groovy'
-    version='0.1'
+    name='Groovy Pygments Lexer',
+    version='0.1',
     author='Matthew Taylor',
-    author_email='matt.taylor@springsource.com'
-    description='Groovy Lexer for Pygments',
+    author_email='matt.taylor@springsource.com',
+    description=__doc__,
     license='BSD',
-    url='http://weblog.dangertree.net/pygments-groovy-lexer'
-    keywords='groovy grails syntax highlighting'
-    packages=find_packages(),
-    entry_points='''
-        [pygments.lexers]
-        groovylexer = lexer.GroovyLexer
+    url='http://weblog.dangertree.net/pygments-groovy-lexer',
+    keywords='groovy grails syntax highlighting',
+    packages=['groovy_lexer'],
+    entry_points='''[pygments.lexers]
+groovylexer = groovy_lexer:GroovyLexer
     '''
 )
